355d9ba8e1d062e870a674c8fe040df3d2955fdd
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-26 21:51:58 -0700

    Add imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { rootReducer } from './reducers/index';
 import './index.css';
 import App from './Containers/App/App.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+
+<Provider store={store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+</Provider>
+
+  , document.getElementById('root'));
 
 serviceWorker.unregister();
