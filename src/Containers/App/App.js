267448703e0cfb34bdda8e4058ea1e23e8e8d357
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-26 19:05:48 -0700

    Add intro component

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Intro from '../Intro/Intro';
 import './App.css';
 
 class App extends Component {
 
   render() {
     return (
-      <h1>MTV MUSIC TRIVIA! 👨‍🎤</h1>
+      <div id='outer-div'>
+        <section id='game-container'>
+          <h1>MTV MUSIC TRIVIA! 👨‍🎤</h1>
+          <Intro />
+        </section>
+      </div>
     )
 
 
