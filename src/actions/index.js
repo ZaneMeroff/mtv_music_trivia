8c955a071ef8d2542c36bec9454f5f208d25d98c
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-27 14:15:21 -0700

    Add actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,13 @@ export const getTriviaData = triviaData => ({
   type: 'GET_TRIVIA_DATA',
   triviaData
 });
+
+export const correctQuestions = question => ({
+  type: 'SAVE_CORRECT_QUESTION',
+  question
+});
+
+export const incorrectQuestions = question => ({
+  type: 'SAVE_INCORRECT_QUESTION',
+  question
+});
