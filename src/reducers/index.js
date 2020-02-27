64f68d2ff3a41f5b8b13c024297110abeda3b942
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-27 16:45:45 -0700

    Modify action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 import { triviaData } from './triviaDataReducer';
 import { correctQuestions } from './correctQuestionsReducer';
 import { incorrectQuestions } from './incorrectQuestionsReducer';
+import { userName } from './userNameReducer';
+import { difficulty } from './difficultyReducer';
 
 export const rootReducer = combineReducers({
   triviaData,
   correctQuestions,
-  incorrectQuestions
+  incorrectQuestions,
+  userName,
+  difficulty
 });
