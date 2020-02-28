cb99d842dca0fac351c694f2ee1040949a167abd
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-27 23:19:07 -0700

    Revise structure

--- src/Containers/Result/Result.js
+++ src/Containers/Result/Result.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './Result.css';
 
 class Result extends Component {
@@ -6,10 +7,10 @@ class Result extends Component {
   render() {
     return (
       <div className='result-contianer'>
-        <p className='result-text'>Way to go Zack!</p>
-        <p className='result-text'>Difficulty: EASY</p>
-        <p className='result-text'>Score: 90%</p>
-        <button className='new-game-button'>NEW GAME</button>
+        <p className='result-text'>Way to go <span id='user-input'>{this.props.username}</span></p>
+        <p className='result-text'>difficulty: <span id='user-input'>{this.props.difficulty}</span></p>
+        <p className='result-text'>score: <span id='user-input'>____</span></p>
+        <button className='new-game-button'>new game</button>
       </div>
     )
 
@@ -17,4 +18,9 @@ class Result extends Component {
 
 }
 
-export default Result;
+export const mapStateToProps = (state) => ({
+  difficulty: state.difficulty,
+  username: state.userName
+});
+
+export default connect(mapStateToProps)(Result);
