cca0b4808fe3d4b7b7207c0f88d5521bd28377c8
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-27 23:21:18 -0700

    Refactor reducer

--- src/reducers/correctQuestionsReducer.js
+++ src/reducers/correctQuestionsReducer.js
@@ -1,7 +1,7 @@
 export const correctQuestions = (state = [], action) => {
   switch(action.type) {
     case 'SAVE_CORRECT_QUESTION':
-      return [...action.correctQuestions]
+      return [...state, action.question]
     default:
       return state;
   }
