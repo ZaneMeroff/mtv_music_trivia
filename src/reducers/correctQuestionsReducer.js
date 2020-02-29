57601e550504c77094f3d8391f08a4fda349109f
Zachary Nemeroff <zanemeroff@gmail.com>
2020-02-29 15:06:04 -0700

    Add case to reducer

--- src/reducers/correctQuestionsReducer.js
+++ src/reducers/correctQuestionsReducer.js
@@ -2,6 +2,8 @@ export const correctQuestions = (state = [], action) => {
   switch(action.type) {
     case 'SAVE_CORRECT_QUESTION':
       return [...state, action.question]
+    case 'CLEAR_CORRECT_QUESTIONS':
+      return state = []
     default:
       return state;
   }
