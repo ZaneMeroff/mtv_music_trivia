7b6a9d5befeb2dae886d4275bc70fa7617b187f7
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-01 19:49:11 -0600

    Updates

---
+++ precache-manifest.0f1a600d620e898e630ac21c545912d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db47cb037282ca4b2a8539b15ff809ee",
+    "url": "/mtv_music_trivia/index.html"
+  },
+  {
+    "revision": "e082d4e7db074cbbfa48",
+    "url": "/mtv_music_trivia/static/css/main.9a603abc.chunk.css"
+  },
+  {
+    "revision": "23ae67939b071383b5f1",
+    "url": "/mtv_music_trivia/static/js/2.cb5deb73.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/mtv_music_trivia/static/js/2.cb5deb73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e082d4e7db074cbbfa48",
+    "url": "/mtv_music_trivia/static/js/main.97312059.chunk.js"
+  },
+  {
+    "revision": "a5ffb6e095120b5d6b26",
+    "url": "/mtv_music_trivia/static/js/runtime-main.0bde4636.js"
+  },
+  {
+    "revision": "f8ca0da6c326f0d072957bdc97b3520e",
+    "url": "/mtv_music_trivia/static/media/liquid_background.f8ca0da6.jpeg"
+  },
+  {
+    "revision": "9ff1150a2f2afce38430fb4be80b95a4",
+    "url": "/mtv_music_trivia/static/media/mtv_logo_pink.9ff1150a.png"
+  },
+  {
+    "revision": "0c1ce2fe11e58ceda0840617e1672aa4",
+    "url": "/mtv_music_trivia/static/media/mtv_logo_yellow.0c1ce2fe.png"
+  }
+]);
